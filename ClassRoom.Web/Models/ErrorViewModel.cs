a80ed1979b7bd7ffd763fce929496b7b81a3c5a9
Quan Hoang <quan.nguyen.se@gmail.com>
2025-03-12 09:11:42 +0700

    Add project files.

---
+++ ClassRoom.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassRoom.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
